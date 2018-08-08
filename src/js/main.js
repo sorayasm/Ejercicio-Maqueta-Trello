66d02dee211e03f032f811bb67706a121aa84cce
SorayaSM <soraya.sacaan@gmail.com>
2018-08-08 18:28:33 -0400

    actualizacion miercoles

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,20 @@
 // Add List
 function newlist() {
-    console.log("boton apretado");
-    document.getElementById("list-button").classList.add("hide");
-    document.getElementById("list-form").classList.remove("hide");
+    console.log("boton nueva lista apretado");
+    document.getElementById("list-button").classList.add("hide"); // hide list button
+    document.getElementById("list-form").classList.remove("hide"); //show list form
+}
+
+function addlist() {
+    console.log("boton agregar lista apretado");
+    let input = document.getElementById("title").value;
+    if (input == "") {
+        alert("Por favor ingresa un nombre para la lista.")
+    } else {
+        document.getElementById("list-form").classList.add("hide"); //hide list form
+        document.getElementById("newlist").style.visibility = "visible"; // show new list
+        const p = document.createElement("p");
+        p.innerHTML = `<p>` + input + `</p>`;
+        newlist.appendChild(p);
+    }
 }
