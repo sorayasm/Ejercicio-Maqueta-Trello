58d0dab07298de78d603469e9b9a4c903c6b778a
SorayaSM <soraya.sacaan@gmail.com>
2018-08-12 15:55:59 -0300

    actualizacion domingo

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,108 @@
-// Add List title
-const btnNewLisTitle = document.getElementById("newlist");
-btnNewLisTitle.addEventListener("click", () => {
-    document.getElementById("listbutton").classList.add("hide"); // hide list button
-    document.getElementById("listform").classList.remove("hide"); //show list form
-});
+// General container
+const container = document.getElementById("container");
 
-// Add list
-const btnAddList = document.getElementById("addlist");
-btnAddList.addEventListener("click", () => {
-    let input = document.getElementById("inputlist").value;
-    if (input == "") {
-        alert("Por favor ingresa un nombre para la lista.")
-    } else {
-        document.getElementById("listform").classList.add("hide"); //hide list form
-        document.getElementById("newlistdisplay").classList.remove("hide"); // show new list
-        //Create new card input
-        const div = document.createElement("div");
-        div.innerHTML = `
-        <p><h1>` + input + `</h1></p>`;
-        titlelist.appendChild(div);
+// Create List
+const divList = document.createElement("div");
+divList.className = "list";
+container.appendChild(divList);
+
+//Create Button New Title list
+const listbutton = document.createElement("div");
+listbutton.id = "listbutton";
+divList.appendChild(listbutton);
+
+const ul = document.createElement("ul");
+ul.className = "nav nav-pills";
+listbutton.appendChild(ul);
+
+const li = document.createElement("li");
+li.id = "newlist";
+li.className = "btn main";
+li.innerHTML = " + Añada una lista ...";
+li.addEventListener("click", newTitle)
+ul.appendChild(li);
+
+//Create Write New Title List 
+function newTitle() {
+    console.log("newTitle")
+    listbutton.classList.add("hide");
+    const listformdiv = document.createElement("div");
+    listformdiv.id = "listform";
+    listformdiv.className = "list-form";
+    divList.appendChild(listformdiv);
+
+    inputlist = document.createElement("input");
+    inputlist.id = ("inputlist");
+    inputlist.className = "form-control";
+    inputlist.type = ("text");
+    inputlist.placeholder = "Introduzca el título de la lista...";
+    listformdiv.appendChild(inputlist);
+
+    const addListbutton = document.createElement("button");
+    addListbutton.id = "addlist";
+    addListbutton.className = "btn btn-success";
+    addListbutton.innerText = "Añadir lista";
+    listformdiv.appendChild(addListbutton);
+
+    addListbutton.addEventListener("click", function() {
+        listformdiv.classList.add("hide");
+        listDisplay();
+    });
+
+
+
+    //Create new list display
+    function listDisplay() {
+        console.log("listDisplay")
+        const listDisplayDiv = document.createElement("div");
+        listDisplayDiv.id = "newlistdisplay";
+        listDisplayDiv.className = "list-form";
+        divList.appendChild(listDisplayDiv);
+
+        let cardDiv = document.createElement("div");
+        cardDiv.id = "titlelist";
+        let input = document.getElementById("inputlist").value;
+        console.log(input);
+        cardDiv.innerHTML = input;
+        listDisplayDiv.appendChild(cardDiv);
+
+        const cardbuttonDiv = document.createElement("div");
+        cardbuttonDiv.id = "cardbutton";
+        listDisplayDiv.appendChild(cardbuttonDiv);
+
+        const ulList = document.createElement("ul");
+        ulList.className = "nav nav-pills";
+        cardbuttonDiv.appendChild(ulList);
+
+        const liList = document.createElement("li");
+        liList.id = "newcard";
+        liList.className = "btn buttons main";
+        liList.innerHTML = "+ Añada una tarjeta";
+        ulList.appendChild(liList);
     }
-});
+}
 
-// Show card input
-const btnNewCard = document.getElementById("newcard");
-btnNewCard.addEventListener("click", () => {
-    document.getElementById("cardbutton").classList.add("hide"); //hide card button
-    document.getElementById("cardform").classList.remove("hide"); //show list form
-});
 
-// Add card and keep the form
-const btnAddCard = document.getElementById("addcard");
-btnAddCard.addEventListener("click", () => {
-    const cardtext = document.getElementById("inputcard").value;
-    if (cardtext == "") {
-        alert("Por favor ingresa un texto para la tarjeta.")
-    } else {
-        const p = document.createElement("p");
-        p.innerHTML = cardtext;
-        textcard.appendChild(p);
-        document.getElementById("inputcard").value = "";
-    }
-});
+
+
+/*
+    // Show card input
+    const btnNewCard = document.getElementById("newcard");
+    btnNewCard.addEventListener("click", () => {
+        document.getElementById("cardbutton").classList.add("hide"); //hide card button
+        document.getElementById("cardform").classList.remove("hide"); //show list form
+    });
+
+    // Add card and keep the form
+    const btnAddCard = document.getElementById("addcard");
+    btnAddCard.addEventListener("click", () => {
+        const cardtext = document.getElementById("inputcard").value;
+        if (cardtext == "") {
+            alert("Por favor ingresa un texto para la tarjeta.")
+        } else {
+            const p = document.createElement("p");
+            p.innerHTML = cardtext + ` <li class="btn buttons delete ">&#xf00d;</li>`;
+            textcard.appendChild(p);
+            document.getElementById("inputcard").value = "";
+        }
+    })*/
